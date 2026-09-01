091394abc7ee4bbc02a5020d3d0c6facc642f934
Sourabh Dwivedi <sourabhdwivedi9690@gmail.com>
2026-09-01 21:01:48 +0530

    sort char by freq

---
+++ 451.py
@@ -0,0 +1,16 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        dit={}
+            
+        for i in s:
+            if i not in dit:
+                dit[i]=s.count(i)
+
+        dit = dict(sorted(dit.items(), key=lambda x: x[1], reverse=True))
+        s=""
+        for k,v in dit.items():
+            s+=k*v
+
+        
+        return s
+        
