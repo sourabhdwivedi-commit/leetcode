377b8c47c12be1accaf59ae2ea8fe0c698b908cb
Sourabh Dwivedi <sourabhdwivedi9690@gmail.com>
2026-09-08 19:07:37 +0530

    max subarray (kadance algo)

--- 53.py
+++ 53.py
@@ -1,23 +1,10 @@
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        best=nums[0]+nums[1]+nums[2]
-        for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            left,right=i+1,len(nums)-1
-            while left<right:
-                curr=nums[i]+nums[left]+nums[right]
-                if abs(curr-target)<abs(best-target):
-                    best=curr
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum=max_sum=nums[0]
+        for x in nums[1:]:
+            curr_sum=max(x,curr_sum+x)
+            max_sum=max(max_sum,curr_sum)
 
-                if curr<target:
-                    left+=1
-                elif curr>target:
-                    right-=1
-                else:
-                    return curr    
-
-        return best                    
+        return max_sum          
 
         
